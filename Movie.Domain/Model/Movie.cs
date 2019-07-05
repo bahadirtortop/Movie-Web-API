53bef5e01f8354f141edfb49dea9748dd9ddbc97
bahadir.tortop <bahadir.tortop@verisoft.com>
2019-07-05 21:31:15 +0300

    Create domain project and movie entity

---
+++ Movie.Domain/Model/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movie.Domain.Model
+{
+    public class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public short Year { get; set; }
+        public DateTime Released { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Language { get; set; }
+        public string Country { get; set; }
+        public string Poster { get; set; }
+        public string ImdbRating { get; set; }
+    }
+}
